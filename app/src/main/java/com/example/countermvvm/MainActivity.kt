5aa72c9a9e83c7ea6753653dde10aa452d1cf2d4
r-elopre <donvgdong052@gmail.com>
2024-02-10 18:40:38 +0800

    this is my third commit

--- app/src/main/java/com/example/countermvvm/MainActivity.kt
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -15,21 +15,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.countermvvm.ui.theme.CounterMVVMTheme
-import com.example.countermvvm.ui.theme.CounterViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val viewModel:CounterViewModel = viewModel()
+            val viewModel: CounterViewModel = viewModel()
             CounterMVVMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
