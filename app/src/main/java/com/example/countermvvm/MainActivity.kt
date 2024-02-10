901c83ffcffd6b1a174ecc35a3607ae63c7bc3da
ri <donvgdong052@gmail.com>
2024-02-10 18:06:28 +0800

    this is sec

--- app/src/main/java/com/example/countermvvm/MainActivity.kt
+++ app/src/main/java/com/example/countermvvm/MainActivity.kt
@@ -3,26 +3,40 @@ package com.example.countermvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.countermvvm.ui.theme.CounterMVVMTheme
+import com.example.countermvvm.ui.theme.CounterViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val viewModel:CounterViewModel = viewModel()
             CounterMVVMTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TheCounterApp(viewModel)
                 }
             }
         }
@@ -30,17 +44,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun TheCounterApp (viewModel: CounterViewModel){
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CounterMVVMTheme {
-        Greeting("Android")
+
+
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Count: ${viewModel.count.value}")
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Button(onClick = { viewModel.increment() }) {
+                Text(text = "Increment")
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+
+            Button(onClick = { viewModel.decrement() }) {
+                Text(text = "Decrement")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/countermvvm/ui/theme/CounterViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.countermvvm.ui.theme
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    private val _count = mutableStateOf(0)
+    val count: MutableState<Int> = _count
+
+    fun increment (){
+        _count.value++
+    }
+
+    fun decrement (){
+        _count.value--
+    }
+}
